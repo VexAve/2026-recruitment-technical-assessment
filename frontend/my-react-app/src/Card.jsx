ef156dac63857503977039636cc858b7cb2fcd0b
Dylan Hans <z5726279@ad.unsw.edu.au>
2026-02-22 19:55:45 +1100

    feat: Card has different layout on smaller screens

--- frontend/my-react-app/src/Card.jsx
+++ frontend/my-react-app/src/Card.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import "./styles/Card.css";
 
 function Card({ name, image, numRooms }) {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 600);
+
+  function updateState() {
+    setIsSmallScreen(window.innerWidth <= 600);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", updateState);
+    return () => window.removeEventListener("resize", updateState);
+  }, []);
+
   return (
     <div className="card" style={{ backgroundImage: `url(${image})` }}>
+      {isSmallScreen && <span className="name">{name}</span>}
       <div className="num-rooms">
         <div className="green-circle"></div>
-        {`${numRooms} rooms available`}
+        {isSmallScreen ? `${numRooms} / ${numRooms}` : `${numRooms} rooms available`}
       </div>
-      <div className="name">{name}</div>
+      {!isSmallScreen && <div className="name">{name}</div>}
     </div>
   );
 }
