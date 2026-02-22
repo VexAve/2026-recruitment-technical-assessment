086aa22ff489e298b39e13da7ba60aee5ca725d1
Dylan Hans <z5726279@ad.unsw.edu.au>
2026-02-22 12:19:12 +1100

    feat: empty div for decorative green circle

--- frontend/my-react-app/src/Card.jsx
+++ frontend/my-react-app/src/Card.jsx
@@ -3,7 +3,10 @@ import "./styles/Card.css";
 function Card({ name, image, numRooms }) {
   return (
     <div className="card" style={{ backgroundImage: `url(${image})` }}>
-      <div className="num-rooms">{`${numRooms} rooms available`}</div>
+      <div className="num-rooms">
+        <div className="green-circle"></div>
+        {`${numRooms} rooms available`}
+      </div>
       <div className="name">{name}</div>
     </div>
   );
